0c93892e87dc2356808a21563ad22d50b073c451
Chenlonggang <chenlonggang.love@163.com>
2014-10-11 11:42:09 +0800

    done

--- main.cpp
+++ main.cpp
@@ -10,40 +10,11 @@ int main(){
 	uchar * p=NULL;
 	csa.extracting(0,20,p);
 	cout<<p<<endl;
-	csa.save("bible.csa");
-
-	CSA csa2;
-	csa2.load("bible.csa");
-	uchar * pp=NULL;
-	csa2.extracting(0,20,pp);
-	cout<<pp<<endl;
-
-	CSA csa3=csa2;
-	int count=0;
-	csa3.counting("the",count);
-	cout<<count<<endl;
-	uchar *ppp=NULL;
-	csa3.extracting(0,20,ppp);
-	cout<<ppp<<endl;
 	
-	csa3=csa;
-	int cishu;
-	csa3.counting("the",cishu);
-	cout<<cishu<<endl;
-	uchar * pppp=NULL;
-	csa3.extracting(0,20,pppp);
-	cout<<pppp<<endl;
 	i32 * pos=NULL;
-	csa3.locating("the",num,pos);
+	csa.locating("the",num,pos);
 	for(i32 i=0;i<10 && i<num;i++)
 		cout<<pos[i]<<endl;
 
-/*
-	CSA csa("bible");
-	uchar *p=NULL;
-	int n=csa.getN();
-	csa.extracting(0,n,p);
-	cout<<p<<endl;
 	return 0;
-*/
 }
